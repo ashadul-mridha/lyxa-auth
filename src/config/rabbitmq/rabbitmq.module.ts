86cc0b6289025313c29c0d5e16eab04b7dd5574d
Ashadul Islam <ashadulmridhaprog@gmail.com>
2025-05-30 11:39:07 +0600

    feat(rabbitmq): setup rabbitmq in the project

---
+++ src/config/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,30 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { RabbitmqService } from './rabbitmq.service';
+dotenv.config();
+
+@Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'auth_api',
+          type: 'topic',
+        },
+        {
+          name: 'auth_rpc',
+          type: 'direct',
+        },
+      ],
+      uri: process.env.RMQ_URL,
+      connectionInitOptions: {
+        wait: false,
+      },
+      enableControllerDiscovery: true,
+    }),
+  ],
+  exports: [RabbitMQModule, RabbitmqService],
+  providers: [RabbitmqService],
+})
+export class RabbitmqModule {}
